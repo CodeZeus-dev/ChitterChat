1601e9d3b1e003845171e515e7d9f8a12a90e131
CodeZeus-dev <constantine.akritides@gmail.com>
2021-03-04 19:23:38 +0000

    added random username generator

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  // console.log('a user connected');
-  // socket.on('disconnect', () => {
-  //   console.log('user disconnected');
-  // });
+  const username = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] });
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', msg, username);
+  });
+  socket.on('disconnect', () => {
+    io.emit('user disconnected');
   });
 });
 
